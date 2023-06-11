613217a7396aea0b6f9539d1d6922ad12c496907
Fernando Vazquez <fernando.vazquez@cartwheelgalaxy.com>
2023-06-11 17:59:05 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,11 +1,9 @@
-function skillMemeber() {
-    var member = document.getElementById("member");
-    var memberValue = member.options[member.selectedIndex].value;
-    var memberText = member.options[member.selectedIndex].text;
-    if (memberValue == "1") {
-        document.getElementById("skillMember").style.display = "block";
-        document.getElementById("skillMember").innerHTML = "Member: " + memberText;
-    } else {
-        document.getElementById("skillMember").style.display = "none";
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
     }
-} 
+  };
+}
